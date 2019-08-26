a42bceec32e81a0ff1cd8a51ebe3cc99a71c4f0c
pray_lee <praycis@gmail.com>
2019-08-26 15:50:37 +0800

    update bind.js

--- src/2019_08_26/bind.js
+++ src/2019_08_26/bind.js
@@ -1,8 +1,9 @@
 // 实现bind函数
 
 // function () {}.bind(obj)
+// 简化版
 Function.prototype.bind1 = function (context) {
-  let _this = this
+  const _this = this
   return function (args) { // 返回一个新函数
     return _this.apply(context, args) // 执行新函数的时候，会把之前传入的context当做新函数体内的this
   }
@@ -15,3 +16,22 @@ const func = function () {
   console.log(this.name)
 }.bind1(obj)
 func()
+
+// 复杂版
+Function.prototype.bind2 = function () {
+  const self = this
+  context = [].shift.call(arguments) // 需要绑定的this上下文
+  args = [].slice.call(arguments) //剩余的参数转成数组
+  return function () {// 返回一个新的函数
+    return self.apply(context, [].concat.call(args, [].slice.call( arguments )))
+  }
+}
+
+const obj1 = {
+  name: 'lee'
+}
+const func1 = function (a, b, c, d) {
+  console.log(this.name)
+  console.log(a, b, c, d)
+}.bind2(obj1, 1, 2)
+func1 (3, 4)
