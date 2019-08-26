a381e5cf1e355ffd0d8a275cba9685b4915e87f9
pray_lee <praycis@gmail.com>
2019-08-26 19:06:22 +0800

    update 高阶函数

--- src/2019_08_26/闭包和高阶函数.js
+++ src/2019_08_26/闭包和高阶函数.js
@@ -26,7 +26,7 @@ var Tv = {
 }
 
 var OpenTvCommand = function (receiver) {
-  this.receiver = receiver
+  this.receiver = receiver // tv
 }
 OpenTvCommand.prototype.execute = function () {
   this.receiver.open() // 执行命令
@@ -71,3 +71,64 @@ var setCommand1 = function (command) {
 }
 
 setCommand1(createCommand(Tv))
+
+
+// 高阶函数 (函数可以作为参数被传递，函数可以作为返回值输出)
+// 把函数当作参数传递, 这代表我们可以抽离出一部分容易变化的业务逻辑, 把这部分业务逻辑放在函数参数中, 这样一来可以分离业务代码中变化与不变的部分
+
+// bad
+var appendDiv = function () {
+  for (var i = 0; i < 100; i++) {
+    var div = document.createElement('div')
+    div.innerHTML = i
+    document.body.appendChild(div)
+    div.style.display = 'none' // 这个可以抽出来，因为这个逻辑可能发生变化,可能是修改其他css样式
+  }
+}
+
+// good
+var appendDiv1 = function (callback) {
+  for (var i = 0; i < 100; i++) {
+    var div = document.createElement('div')
+    div.innerHTML = i
+    document.body.appendChild(div)
+    if (typeof callback === 'function') {
+      callback(div) // 这样就把这块的逻辑抽离出来了，以后直接修改回调函数就可以了
+    }
+  }
+}
+// appendDiv1(function (node) {
+//   node.style.display = 'none'
+// })
+
+
+// 高阶函数AOP demo (面向切面编程)
+Function.prototype.before = function (beforeFn) {
+  var _this = this // 保存原函数的引用 
+  return function () { // 返回包含了原函数和新函数的'代理函数'
+    beforeFn.apply(this, arguments) // 执行新函数, 修正this
+    console.log(5)
+    return _this.apply(this, arguments) // 执行原函数
+  }
+}
+Function.prototype.after = function (afterFn) {
+  var _this = this
+  return function () {
+    // 这里才开始执行beforeFn返回的函数
+    var ret = _this.apply(this, arguments)
+    afterFn.apply(this, arguments)
+    console.log(ret)
+    return ret
+  }
+}
+
+var func = function () {
+  console.log(2)
+  return 111
+}
+func = func.before(function() {
+  console.log(1)
+}).after(function() {
+  console.log(3)
+})
+func()
