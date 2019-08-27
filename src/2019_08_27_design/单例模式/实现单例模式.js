613d77adceca49a35af9c5d4d1a175eff34eee59
pray_lee <praycis@gmail.com>
2019-08-27 16:30:09 +0800

    create singleton

---
+++ src/2019_08_27_design/单例模式/实现单例模式.js
@@ -0,0 +1,32 @@
+var Singleton = function (name) {
+  this.name = name
+  this.instance = null
+}
+
+Singleton.prototype.getName = function () {
+  console.log(this.name)
+}
+Singleton.getInstance = function (name) {
+  console.log(this.instance)
+  console.log(1)
+  if (!this.instance) {
+    this.instance = new Singleton(name)
+  }
+  return this.instance
+}
+var a = Singleton.getInstance('sven1')
+var b = Singleton.getInstance('sven2')
+console.log(a === b) // true
+
+Singleton.getInstance1 = (function () {
+  var instance = null
+  return function (name) {
+    if(!instance) {
+      instance = new Singleton(name)
+    }
+    return instance
+  }
+})()
+var c = Singleton.getInstance1('sven1')
+var d = Singleton.getInstance1('sven2')
+console.log(c === d) // true
