dbfb4ab5f37473877764c0683b69c8aee9bde3e5
pray_lee <praycis@gmail.com>
2019-08-29 14:31:31 +0800

    update

---
+++ src/2019_08_27_design/单例模式/惰性单例模式.js
@@ -0,0 +1,23 @@
+// 惰性单例是指在需要的时候才创建对象实例
+// 通用的惰性单例
+var getSingle = function (fn) {
+  var result
+  return function () {
+    return result || (result = fn.apply(this, arguments))
+  }
+}
+
+var createLoginLayer = function () {
+  // logic
+  return 'createLoginLayer'
+}
+var createOtherLayer = function () {
+  // logic
+  return 'createOtherLayer'
+}
+
+var createSingleLoginLayer = getSingle(createLoginLayer)
+var createSingleLoginLayer1 = getSingle(createLoginLayer)
+var a = createSingleLoginLayer()
+var b = createSingleLoginLayer1()
+console.log(a === b)
